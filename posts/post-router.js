427fbae2cbee8b29b9c8067d2e6a7285547a070d
Rory Murray <rory.christian.murray@gmail.com>
2020-02-05 16:15:01 -0500

    GET by id

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,21 +6,25 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-
     try {
         const posts = await db('posts');
         // const posts = await db.select('*').from('posts');
         res.json(posts)
     } catch(err) {
         res.status(500).json({message: "failed to get posts"})
-
     }
-
-
-
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        // const post = await db('posts').where('id', id)
+        // destructure array to get object out
+        const [post] = await db('posts').where('id', id)
+        res.status(200).json(post)
+    } catch(err) {
+        res.status(500).json({message: "failed to get post by id"})
+    }
 
 });
 
